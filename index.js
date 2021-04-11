11c0285cb20922d74d7ea42020362794f4e3c572
Anang Fachreza <fachrezanang@gmail.com>
2021-04-11 11:55:13 +0700

    change configuration

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ module.exports = {
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  semi: true,
+  singleQuote: false,
+  trailingComma: "none",
   bracketSpacing: false,
-  jsxBracketSameLine: false
+  arrowParens: "avoid"
 }
